3af6aadf3513e783dc9165a19741848ec35a1ae3
Alan Patterson <alan.f.patterson@nasa.gov>
2012-05-18 08:24:13 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'adfd150edd0c84d6333762923c84408e0e3c6fe965a821bc1d8aa01e294befdfcc5aa998864a125791100fcf780e28014cfd048523e221daedd430dfd44c9b73'
